f2bfb2b603403073d91ccad16b2f4c20f39bff55
XandyGomes <x3.computadores@hotmail.com>
2026-03-02 20:24:09 -0300

    feat: add login functionality and navigation setup
    - Implemented login page with email and password input
    - Added navigation to main page upon successful login
    - Created main page skeleton and API service for GitHub
    - Set up routing with React Navigation
    - Updated package.json with necessary dependencies

---
+++ src/pages/login.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigation = useNavigation();
+
+  const handleLogin = () => {
+    if (email === "xandy@gmail.com" && password === "123") {
+      navigation.navigate("main");
+    } else {
+      Alert.alert("E-mail ou senha inválidos!");
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="E-mail"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        value={password}
+        secureTextEntry={true}
+        onChangeText={setPassword}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>ENTRAR</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#FFF",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#CCC",
+    borderRadius: 5,
+    padding: 10,
+    marginVertical: 10,
+    width: "80%",
+  },
+  button: {
+    backgroundColor: "#9705f9c2",
+    borderRadius: 5,
+    padding: 10,
+    width: "80%",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+});
+
+export default Login;

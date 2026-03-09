612219146b96b081765b1cd814e3fdb5feffefec
XandyGomes <x3.computadores@hotmail.com>
2026-03-09 19:12:19 -0300

    feat: implement user management functionality with AsyncStorage integration

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,10 +1,59 @@
-import React, {Component} from "react";
-import {Keyboard, ActivityIndicator} from "react-native"
-import Icon from "@expo/vector-icons/MaterialIcons"
+import React, { Component } from "react";
+import { Keyboard, ActivityIndicator } from "react-native";
+import Icon from "@expo/vector-icons/MaterialIcons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import Api from "../services/api"
-import {  } from "../styles"
+import Api from "../services/api";
+import {} from "../styles";
 
-export default class Main extends Component{
+export default class Main extends Component {
+  state = {
+    newUser: "",
+    users: [],
+    loading: false,
+  };
 
+  async componentDidMount() {
+    const users = await AsyncStorage.getItem("users");
+    if (users) {
+      this.setState({ users: JSON.parse(users) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { users } = this.state;
+    if (prevState.users !== users) {
+      AsyncStorage.setItem("users", JSON.stringify(users));
+    }
+  }
+
+  handleAdduser = async () => {
+    try {
+      const { users, newUser } = this.state;
+      this.setState({ loading: true });
+
+      const response = await Api.get(`/users/${newUser}`);
+      if (users.find((user) => user.login === response.data.login)) {
+        alert("Usuário já adicionado!");
+        this.setState({ loading: false });
+        return;
+      }
+
+      const data = {
+        name: response.data.name,
+        login: response.data.login,
+        bio: response.data.bio,
+        avatar: response.data.avatar_url,
+      };
+
+      this.setState({
+        users: [...users, data],
+        newUser: "",
+        loading: false,
+      });
+
+      Keyboard.dismiss();
+    } catch (error) {
+        
+    }
+  };
 }

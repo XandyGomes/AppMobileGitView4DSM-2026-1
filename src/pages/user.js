2414e3428abbc19a4cb34c4f6493d7e105d1eb9c
XandyGomes <x3.computadores@hotmail.com>
2026-03-17 20:03:57 -0300

    refactor: clean up code formatting and improve readability in user component

--- src/pages/user.js
+++ src/pages/user.js
@@ -1,35 +1,60 @@
 import React, { Component } from "react";
-import api from "../services/api"
+import api from "../services/api";
 import {
-    Container,
-    Header,
-    AvatarPerfil,
-    NamePerfil,
-    BioPerfil,
-    Stars,
-    Starred,
-    OwnerAvatar,
-    Info,
-    Title,
-    Author,
-} from "../styles.js"
+  Container,
+  Header,
+  AvatarPerfil,
+  NamePerfil,
+  BioPerfil,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from "../styles.js";
 
 export default class User extends Component {
-    state = {
-        stars: []
-    }
+  state = {
+    stars: [],
+  };
 
-    async componentDidMount(){
-        const { route } = this.props;
-        const { user } = route.params;
+  async componentDidMount() {
+    const { route } = this.props;
+    const { user } = route.params;
 
-        const response = await api.get(`/users/${user.login}/starred`)
-        this.setState({ stars: response.data })
-    }
+    const response = await api.get(`/users/${user.login}/starred`);
+    this.setState({ stars: response.data });
+  }
 
-    render(){
-        
-        return()
-    }
+  render() {
+    const { route } = this.props;
+    const { user } = route.params;
+    const { stars } = this.state;
 
+    return (
+      <Container>
+        <Header>
+          <AvatarPerfil source={{ uri: user.avatar }} />
+          <NamePerfil>{user.name}</NamePerfil>
+          <BioPerfil>{user.bio}</BioPerfil>
+        </Header>
+
+        <Stars
+          showsVerticalScrollIndicator={false}
+          data={stars}
+          keyExtractor={(star) => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
+    );
+  }
 }
